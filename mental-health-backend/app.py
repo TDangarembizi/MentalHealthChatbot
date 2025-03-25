b2fd39c067be2470d0fa854edc4a92398fbbeea3
Tino <T0410828@my.ntu.ac.uk>
2025-03-25 22:14:30 +0000

    initial

--- mental-health-backend/app.py
+++ mental-health-backend/app.py
@@ -7,13 +7,23 @@ from firebase_admin import credentials, auth
 app = Flask(__name__)
 CORS(app)  # Allow requests from React frontend (http://localhost:3000 by default)
 
-# Load Firebase Admin credentials
+from dotenv import load_dotenv
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-cred_path = os.path.join(BASE_DIR, "firebase-service-account.json")
-cred = credentials.Certificate(cred_path)
 
-firebase_admin.initialize_app(cred)
+load_dotenv()  # Load .env into environment
+
+firebase_config = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+}
 
 @app.route("/")
 def index():
